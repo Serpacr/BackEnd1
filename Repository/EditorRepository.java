a6b9e6cdf6479d1a207523371eb5273ccc107c1b
Serpacr <164097767+Serpacr@users.noreply.github.com>
2025-03-25 21:44:04 -0300

    Add files via upload

---
+++ Repository/EditorRepository.java
@@ -0,0 +1,7 @@
+package com.seuprojeto.miniprojeto.Repository;
+
+import com.seuprojeto.miniprojeto.Model.Editor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EditorRepository extends JpaRepository<Editor, Long> {
+}
